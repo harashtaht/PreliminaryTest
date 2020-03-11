8c7372a0839330b77590a5013ad36dc062617abd
HHT <harashtatandjung@gmail.com>
2020-03-11 20:09:17 +0700

    SQL 02

--- Programming/rotate_box.py
+++ Programming/rotate_box.py
@@ -1,6 +1,20 @@
-#Information
+### Information ###
+# Box contains food packages.
+# Size of the box is length (l) * 2 * height (h)
+# Size of the food package is 2*2*2 (size of each box)
+# Box is represented as array A
+# During delivery box is rotated "k" times 90 degrees clockwise rotation
+# K is inputted
 
-# Code Input
+# Code Input - Must separate each string, append to list
+
+# Input
+# 6 -> length
+# 2 2 1 3 1 1 0-> elements of array
+# 1 -> number f rotation
+
+# Output
+# 6 3 1
 
 # Code constraints
 
@@ -8,3 +22,12 @@
 length_n = 6
 elements_a = [2,2,1,3,1,1]
 rotate_k = 1
+
+# print(type(length_n))
+# print(type(elements_a))
+# print(type(rotate_k))
+
+# list_1 = [1, [2,3], 5, [7,8,9], 10]
+
+# print(list_1[1][0])
+# print(list_1[3][1:])
